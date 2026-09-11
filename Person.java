f24858434f0c60d1a8529e1cbc946412c95af2f3
Jac123-glitch <moreno.jacqueline@student.ccm.edu>
2026-09-11 14:09:27 -0400

    Week2-lab2-Person

--- Person.java
+++ Person.java
@@ -1,3 +1,41 @@
 public class Person {
+   private String name;
+   private int age;
+   private String email;
+
     
+   public void setName(String n){
+    name = n;
+   }
+
+   public String getName(){
+    return name;
+   }
+    
+    public void setAge(int a){
+        age = a;
+    }
+
+    public int getAge(){
+        return age;
+    }
+  
+    public void setEmail(String e){
+        email = e;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public static void displayInfo(){
+    System.out.println("The Animal is "+peep1.getAnimal());
+    System.out.println("The Age is "+peep1.getAge());
+    System.out.println("The Name is "+peep1.getName());
+    System.out.println("The Animal is "+peep2.getAnimal());
+    System.out.println("The Age is "+peep2.getAge());
+    System.out.println("The Name is "+peep2.getName());
+
+    }
+
 }
--- PersonTest.java
+++ PersonTest.java
@@ -1,3 +1,23 @@
 public class PersonTest {
+
+    public static void main(String [] args){
+        Person peep1 = Person();
+        Person peep2 = Person();
+
+    peep1.setAge(24);
+    peep1.setEmail("emily123@gmail.com");
+    peep1.setName("Emily");
+    
+    peep2.setAge(19);
+    peep2.setEmail("Sofiathefirst@gmail.com");
+    peep2.setName("Sofia"); 
+
+    
+    
+
+
+
+
+    }
     
 }
